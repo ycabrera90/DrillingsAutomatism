a6c9014bbb18bfb1ca0b6a44103cdcf28c34c9a7
yosniel <yosniel.ch@gmail.com>
2022-02-07 18:36:27 -0300

    the flag-icon and the badge were added to QuickView

--- src/pages/QuickView/QuickView.js
+++ src/pages/QuickView/QuickView.js
@@ -4,20 +4,90 @@ import classes from "./QuickView.module.css";
 import QuickViewItem from "./QuickViewItem/QuickViewItem";
 
 const QuickView = (props) => {
-  const quickViewItem = [{ title: "RIVPERF22-PERF 01", workinkMode: "AUTO" }];
+  const systemDatas = [
+    {
+      id: "p1",
+      title: "RIVPERF22-PERF 01",
+      workinkMode: "AUTO",
+      claims: [
+        { id: "c1", active: true },
+        { id: "c2", active: true },
+      ],
+      measures: {
+        tank: [
+          { title: "Altura", value: 1.61, unit: "mts" },
+          { title: "Porciento", value: 28.3, unit: "%" },
+          { title: "Volumen", value: 22, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+        ],
+        drill: [
+          { title: "Presión", value: 1.25, unit: "bar" },
+          { title: "Caudal", value: 15.2, unit: "m3/h" },
+          { title: "Nivel del pozo", value: -12, unit: "m" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+        ],
+      },
+    },
+    {
+      id: "p2",
+      title: "RIVPERF23-PERF 02",
+      workinkMode: "LOCAL",
+      claims: [],
+      measures: {
+        tank: [
+          { title: "Altura", value: 1.61, unit: "mts" },
+          { title: "Porciento", value: 28.3, unit: "%" },
+          { title: "Volumen", value: 22, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+        ],
+        drill: [
+          { title: "Presión", value: 1.25, unit: "bar" },
+          { title: "Caudal", value: 15.2, unit: "m3/h" },
+          { title: "Nivel del pozo", value: -12, unit: "m" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+        ],
+      },
+    },
+    {
+      id: "p3",
+      title: "RIVPERF24-PERF 03",
+      workinkMode: "RAMOTO",
+      claims: [],
+      measures: {
+        tank: [
+          { title: "Altura", value: 1.61, unit: "mts" },
+          { title: "Porciento", value: 28.3, unit: "%" },
+          { title: "Volumen", value: 22, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+        ],
+        drill: [
+          { title: "Presión", value: 1.25, unit: "bar" },
+          { title: "Caudal", value: 15.2, unit: "m3/h" },
+          { title: "Nivel del pozo", value: -12, unit: "m" },
+          { title: "Dummy", value: 11, unit: "m3" },
+          { title: "Dummy", value: 11, unit: "m3" },
+        ],
+      },
+    },
+  ];
+
+  const quickviewItems = systemDatas.map((item) => (
+    <QuickViewItem key={item.id} data={item}></QuickViewItem>
+  ));
 
   return (
     <>
       <section className={classes.background} />
       <section className={classes["quickview-container"]}>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
-        <QuickViewItem data={quickViewItem}></QuickViewItem>
+        {quickviewItems}
       </section>
     </>
   );
--- src/pages/QuickView/QuickViewItem/QuickViewItem.js
+++ src/pages/QuickView/QuickViewItem/QuickViewItem.js
@@ -2,47 +2,71 @@ import React from "react";
 import classes from "./QuickViewItem.module.css";
 
 import { BsCheckCircle } from "react-icons/bs";
+import { FaFlagCheckered } from "react-icons/fa";
 import ItemImage from "../ItemImage/ItemImage";
 import iconTank from "../../../images/icon-tank.png";
 import Data from "../Data/Data";
 import pump from "../../../images/pump.png";
 
-const QuickViewItem = () => {
+const QuickViewItem = (props) => {
+  const { claims, title, workinkMode, measures } = props.data;
+
+  const { status: areActiveClaims, amount: amountOfActiveClaims } =
+    claims.reduce(
+      (prevValue, currValue) =>
+        currValue.active
+          ? { status: true, amount: prevValue.amount + 1 }
+          : prevValue,
+      { status: false, amount: 0 }
+    );
+
+  let renderId = 0;
+
+  const tankMeasuresItems = measures.tank.map((item) => (
+    <Data
+      key={renderId++}
+      title={item.title}
+      value={item.value}
+      unit={item.unit}
+    />
+  ));
+
+  const drillMeasuresItems = measures.drill.map((item) => (
+    <Data
+      key={renderId++}
+      title={item.title}
+      value={item.value}
+      unit={item.unit}
+    />
+  ));
+
   return (
     <article className={classes["item-container"]}>
       <header>
-        <h1>RIVPERF22-PERF 01</h1>
-        <span>AUTO</span>
-        <BsCheckCircle className={classes.icon} />
+        <h1>{title}</h1>
+        <span>{workinkMode}</span>
+        {areActiveClaims && (
+          <span className={classes["claim-badge"]}>{amountOfActiveClaims}</span>
+        )}
+        {areActiveClaims && (
+          <FaFlagCheckered className={classes["flag-icon"]} />
+        )}
+        {!areActiveClaims && (
+          <BsCheckCircle className={classes["checked-icon"]} />
+        )}
       </header>
       <main>
         <article className={classes.item}>
           <i>
             <ItemImage src={iconTank} description="Tanque" />
           </i>
-          <div className={classes["data-container"]}>
-            <Data title="Altura" value={1.61} unit="mts" />
-            <Data title="Porciento" value={28.3} unit="%" />
-            <Data title="Volumen" value={21} unit="m3" />
-            <Data title="Volumen" value={21} unit="m3" />
-            <Data title="Volumen" value={21} unit="m3" />
-            <Data title="Volumen" value={21} unit="m3" />
-            <Data title="Volumen" value={21} unit="m3" />
-          </div>
+          <div className={classes["data-container"]}>{tankMeasuresItems}</div>
         </article>
         <article className={classes.item}>
           <i>
             <ItemImage src={pump} description="Bomba" />
           </i>
-          <div className={classes["data-container"]}>
-            <Data title="Presión" value={1.25} unit="bar" />
-            <Data title="Caudal" value={15.2} unit="m3/h" />
-            <Data title="Nivel Pozo" value={-12} unit="m" />
-            <Data title="Nivel Pozo" value={-12} unit="m" />
-            <Data title="Nivel Pozo" value={-12} unit="m" />
-            <Data title="Nivel Pozo" value={-12} unit="m" />
-            <Data title="Nivel Pozo" value={-12} unit="m" />
-          </div>
+          <div className={classes["data-container"]}>{drillMeasuresItems}</div>
         </article>
       </main>
     </article>
